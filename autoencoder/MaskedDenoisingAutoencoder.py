163f15906818d17fd00093a3fc9148d3e3fa4741
Giodiro <famigliameanti@hotmail.com>
2017-06-19 15:13:48 +0200

    Added comments and cleaned code

--- autoencoder/MaskedDenoisingAutoencoder.py
+++ autoencoder/MaskedDenoisingAutoencoder.py
@@ -3,13 +3,16 @@ from __future__ import print_function, division, absolute_import
 import tensorflow as tf
 import numpy as np
 
+"""File only contains the class for the denoising autoencoder.
 
+@author: gmeanti
+"""
 
 class DenoisingAutoencoder:
     """Implementation of a denoising autoencoder for collaborative filtering
     This class is quite generic in that it implements 3 types of noise
      - Gaussian noise
-     - Masking noise
+     - Masking noise (particularly important for the CF problem)
      - Salt and Pepper noise
     which can be applied to the input in different proportions.
     As already done in https://arxiv.org/pdf/1603.00806.pdf
@@ -18,13 +21,14 @@ class DenoisingAutoencoder:
     in the gradient propagation procedure.
     However no optimizations taking advantage of the input sparsity have been applied (TODO)
 
+
     Transfer function: tanh
     Layers: 1 hidden layer of size `n_hidden`
     """
     def __init__(self, name, n_input, n_hidden, dropout_prob,
                  gaussian_prob, gaussian_std, sap_prob,
                  alpha_weight, beta_weight, regl_weight,
-                 batch, optimizer):
+                 batch, rseed, optimizer):
         """Initialize the weights and the graph for the autoencoder
         @param name: identifier of this neural network used for grouping the 
                 emitted summaries
@@ -40,22 +44,17 @@ class DenoisingAutoencoder:
                 non-dropped out ratings.
         @param regl_weight: regularization applied to the weight matrix
         @param batch: tensorflow variable used to indicate the current data batch
+        @param rseed: random seed for various noise sources
         @param optimizer: the tensorflow optimizer to use for backpropagation
         """
-        self.n_input = n_input
-        self.n_hidden = n_hidden
-        self.name = name
+        self.n_input           = n_input
+        self.n_hidden          = n_hidden
+        self.name              = name
+        self.keep_prob_val     = 1 - dropout_prob
+        self.gaussian_prob_val = gaussian_prob
+        self.sap_prob_val      = sap_prob
 
         self.weights = self._init_weights()
-
-        """
-        In the input there will be many 0 values (missing) and some actual values.
-        We don't care about the 0 values, but want to perform dropout on the actual 
-        values. The loss function differs depending on whether we are talking about
-        dropped out values (weighted with alpha) or kept values (weighted with beta),
-        but it does not take into consideration the missing values.
-        See https://arxiv.org/pdf/1603.00806.pdf for more details
-        """
 
         # Input data
         self.input         = tf.placeholder(tf.float32, [None, self.n_input], name="input")
@@ -64,44 +63,40 @@ class DenoisingAutoencoder:
 
         # Store the parameters in tensorflow constants.
         # dropout probability is converted to keep probability to simplify calculations below
-        self.keep_prob     = tf.constant(1-dropout_prob, dtype=tf.float32, shape=[], name="keep_probability")
-        self.gaussian_prob = tf.constant(gaussian_prob, dtype=tf.float32, shape=[], name="gaussian_noise_probability")
-        self.sap_prob      = tf.constant(sap_prob, dtype=tf.float32, shape=[], name="SAP_probability")
+        self.keep_prob     = tf.placeholder(tf.float32, shape=[], name="keep_probability")
+        self.gaussian_prob = tf.placeholder(tf.float32, shape=[], name="gaussian_noise_probability")
+        self.sap_prob      = tf.placeholder(tf.float32, shape=[], name="SAP_probability")
         self.alpha         = tf.constant(alpha_weight, dtype=tf.float32, shape=[], name="alpha")
         self.beta          = tf.constant(beta_weight, dtype=tf.float32, shape=[], name="beta")
         self.regl          = tf.constant(regl_weight, dtype=tf.float32, shape=[], name="regularization")
-
-        zero = tf.constant(0, dtype=tf.float32)
-        one = tf.constant(1, dtype=tf.float32)
+        self.rseed         = rseed
 
         input_shape = self.input.get_shape()
-        input_type = self.input.dtype
+
+        # Masking noise mask is computed here since we need the binary tensor for
+        # computing the loss function. The procedure is same as for gaussian noise.
         # uniform [keep_prob, 1.0 + keep_prob)
-        random_tensor = self.keep_prob
-        random_tensor += tf.random_uniform(tf.shape(self.input),
-                                           seed=None,
-                                           dtype=input_type)
+        random_tensor = self.keep_prob + tf.random_uniform(tf.shape(self.input), seed=self.get_rseed(), dtype=tf.float32)
         # 0. if [keep_prob, 1.0) and 1. if [1.0, 1.0 + keep_prob)
         binary_tensor = tf.floor(random_tensor)
 
+        # Apply noise to the input.
         with tf.name_scope("noise"):
-            # Apply noise to the input.
-            self.noisy_input = self.add_gaussian_noise(self.input,
-                                                       self.gaussian_prob,
-                                                       gaussian_std)
-            self.noisy_input = self.add_salt_and_pepper_noise(self.noisy_input,
-                                                              self.sap_prob)
-            
+            self.noisy_input = self.add_gaussian_noise(self.input, self.gaussian_prob, gaussian_std)
+            self.noisy_input = self.add_salt_and_pepper_noise(self.noisy_input, self.sap_prob)
+            # Finish applying masking noise. Regularization is performed
             self.noisy_input = tf.div(self.noisy_input, self.keep_prob) * binary_tensor
             self.noisy_input.set_shape(input_shape)
 
+        # Calculate the alpha mask (true for dropped out ratings) and beta mask
+        # (true for non-dropped out ratings)
         with tf.name_scope("masks"):
             # True if a rating is valid and it has been dropped out
-            self.alpha_mask = tf.logical_and(tf.equal(binary_tensor, zero),
+            self.alpha_mask = tf.logical_and(tf.equal(binary_tensor, 0),
                                              tf.logical_not(self.input_mask))
             self.alpha_mask.set_shape(input_shape)
             # True if a rating is valid and it has not been dropped out
-            self.beta_mask = tf.logical_and(tf.equal(binary_tensor, one),
+            self.beta_mask = tf.logical_and(tf.equal(binary_tensor, 1),
                                             tf.logical_not(self.input_mask))
             self.beta_mask.set_shape(input_shape)
             
@@ -112,9 +107,9 @@ class DenoisingAutoencoder:
                 tf.reduce_sum(tf.cast(self.beta_mask, tf.int32)),
                 collections=[self.name])
         
+        # Calculate hidden and reconstruction layers of the NN
         with tf.name_scope("activations"):
             # Encoder
-            #self.noisy_input = tf.tanh(self.noisy_input)
             self.hidden = tf.nn.tanh(tf.add(
                             tf.matmul(self.noisy_input, self.weights['W1']),
                             self.weights['b1']))
@@ -122,6 +117,7 @@ class DenoisingAutoencoder:
             self.reconstruction = tf.nn.tanh(tf.add(
                             tf.matmul(self.hidden, self.weights['W2']), 
                             self.weights['b2']))
+            # Summaries
             tf.summary.histogram('reconstruction', 
                 tf.boolean_mask(self.reconstruction, 
                                 tf.logical_not(self.input_mask)), collections=[self.name])
@@ -129,8 +125,8 @@ class DenoisingAutoencoder:
             tf.summary.histogram('input',
                 tf.boolean_mask(self.input,
                                 tf.logical_not(self.input_mask)), collections=[self.name])
-            #print("Reconstruction shape: %s" % (self.reconstruction.get_shape().as_list()))
 
+        # Calculate the different components of the loss function
         with tf.name_scope("loss"):
             # Calculate loss
             self.alpha_cost = tf.reduce_sum(tf.squared_difference(
@@ -139,7 +135,7 @@ class DenoisingAutoencoder:
             self.beta_cost  = tf.reduce_sum(tf.squared_difference(
                                     tf.boolean_mask(self.reconstruction, self.beta_mask),
                                     tf.boolean_mask(self.input, self.beta_mask))) * self.beta
-            # Regularization
+            # Apply regularization on the magnitude of the weight matrices
             self.regl_cost = self.regl * tf.add(tf.nn.l2_loss(self.weights['W1']),
                                                 tf.nn.l2_loss(self.weights['W2']))
             self.cost = self.alpha_cost + self.beta_cost + self.regl_cost
@@ -149,15 +145,17 @@ class DenoisingAutoencoder:
             tf.summary.scalar('regl_cost', self.regl_cost, collections=[self.name])
             tf.summary.scalar('cost', self.cost, collections=[self.name])
 
+        # Calculate the accuracy (RMSE) on validation data.
+        # This is not executed at every iteration step, only when requested for progress updates
         with tf.name_scope("accuracy"):
-            self.train_err = self.rmse(self.reconstruction, self.input, self.input_mask, "train_predictions")
+            self.train_err = self._rmse(self.reconstruction, self.input, self.input_mask, "train_predictions")
             tf.summary.scalar("train_error", self.train_err, collections=[self.name])
 
             self.test_input = tf.placeholder(tf.float32, [None, self.n_input],
                     name="test_input")
             self.test_input_mask = tf.placeholder(tf.bool, [None, self.n_input], 
                     name="test_input_mask") # True when input is invalid
-            self.test_err = self.rmse(self.reconstruction, self.test_input, self.test_input_mask, "test_predictions")
+            self.test_err = self._rmse(self.reconstruction, self.test_input, self.test_input_mask, "test_predictions")
             tf.summary.scalar("test_error", self.test_err, collections=[self.name])
 
         with tf.name_scope("optimizer"):
@@ -166,17 +164,29 @@ class DenoisingAutoencoder:
         self.summary_op = tf.summary.merge_all(key=self.name)
 
 
-    def add_gaussian_noise(self, X, noise_prob, std):
+    def add_gaussian_noise(self, data, noise_prob, std):
+        """ Add random gaussian noise to a fraction of the data
+        Algorithm:
+        The choice of which elements to apply the noise is done by sampling
+        a tensor from a uniform distribution in range [noise_prob, 1 + noise_prob)
+        then with a floor operation this is converted to a binary tensor where
+        there are on average the correct amount of ones (defined by noise_prob).
+        Finally the a tensor containing gaussian noise is multiplied to the binary tensor
+        to select only the correct portion of elements, and then added to the data tensor.
+        """
         if noise_prob == 0.0:
-            return X
+            return data
         random_tensor = noise_prob
-        random_tensor += tf.random_uniform(shape=tf.shape(X), seed=None, dtype=X.dtype)
+        random_tensor += tf.random_uniform(shape=tf.shape(data), seed=self.get_rseed(), dtype=data.dtype)
         binary_tensor = tf.floor(random_tensor)
-        noise_tensor = tf.random_normal(shape=tf.shape(X), mean=0.0, stddev=std, dtype=X.dtype) * \
+        noise_tensor = tf.random_normal(shape=tf.shape(data), mean=0.0, stddev=std, seed=self.get_rseed(), dtype=data.dtype) * \
                         binary_tensor
-        return X + noise_tensor
+        return data + noise_tensor
+
 
     def add_salt_and_pepper_noise(self, X, noise_prob):
+        """ Add random salt and pepper noise to a fraction of the data
+        """
         if noise_prob == 0.0:
             return X
         mn = tf.reduce_min(X)
@@ -185,7 +195,7 @@ class DenoisingAutoencoder:
         half_noise_prob = 1 - noise_prob/2.0
 
         min_tensor = tf.floor(half_noise_prob + 
-                            tf.random_uniform(shape=tf.shape(X), seed=None, dtype=X.dtype))
+                            tf.random_uniform(shape=tf.shape(X), seed=self.get_rseed(), dtype=X.dtype))
         min_indices = tf.to_int32(
             tf.where(tf.equal(min_tensor, tf.constant(1, dtype=X.dtype))))
         num_true = tf.shape(min_indices)[0]
@@ -194,7 +204,7 @@ class DenoisingAutoencoder:
                    tf.scatter_nd(min_indices, tf.fill([num_true], mn), shape=tf.shape(X)))
 
         max_tensor = tf.floor(half_noise_prob + 
-                            tf.random_uniform(shape=tf.shape(X), seed=None, dtype=X.dtype))
+                            tf.random_uniform(shape=tf.shape(X), seed=self.get_rseed(), dtype=X.dtype))
         max_indices = tf.to_int32(
             tf.where(tf.equal(max_tensor, tf.constant(1, dtype=X.dtype))))
         num_true = tf.shape(max_indices)[0]
@@ -206,6 +216,9 @@ class DenoisingAutoencoder:
 
 
     def _init_weights(self):
+        """Initialize the input->hidden and hidden->reconstruction weight matrixes
+        Also a bias vector is included in the weights.
+        """
         weights = {}
         with tf.name_scope("hidden_layer"):
             weights["W1"] = tf.get_variable('W1', 
@@ -232,7 +245,7 @@ class DenoisingAutoencoder:
 
 
     def variable_summaries(self, var, name):
-      """Attach a lot of summaries to a Tensor."""
+      """Add different summaries related to a variable"""
       with tf.name_scope('summaries'):
         mean = tf.reduce_mean(var)
         tf.summary.scalar('mean/' + name, mean, collections=[self.name])
@@ -244,53 +257,68 @@ class DenoisingAutoencoder:
         tf.summary.histogram(name, var, collections=[self.name])
 
 
-    def partial_fit(self, sess, input, input_mask):
+    def fit(self, sess, input, input_mask):
+        """Perform a single iteration of back-propagation. """
         fd = {
-            self.input: input,
-            self.input_mask: input_mask,
+            self.input:         input,
+            self.input_mask:    input_mask,
+            self.keep_prob:     self.keep_prob_val,
+            self.gaussian_prob: self.gaussian_prob_val,
+            self.sap_prob:      self.sap_prob_val,
         }
-        cost, opt, predictions = sess.run((self.cost, self.optimizer, self.reconstruction),
-                             feed_dict = fd)
-        return cost, predictions
+        cost, opt = sess.run((self.cost, self.optimizer), feed_dict = fd)
+        return cost
 
 
-    def partial_fit_summary(self, sess, input, input_mask, test, test_mask):
+    def fit_summary(self, sess, input, input_mask, test, test_mask):
+        """Obtain summaries and accuracy results. 
+        For accuracy results we need to feed the validation data as well
+        """
         fd = {
-            self.input: input,
-            self.input_mask: input_mask,
-            self.test_input: test,
+            self.input:           input,
+            self.input_mask:      input_mask,
+            self.test_input:      test,
             self.test_input_mask: test_mask,
+            self.keep_prob:       self.keep_prob_val,
+            self.gaussian_prob:   self.gaussian_prob_val,
+            self.sap_prob:        self.sap_prob_val,
         }
 
-        cost, predictions, trerr, tserr, summary = sess.run(
-            (self.cost, self.reconstruction, self.train_err, self.test_err,
-             self.summary_op), feed_dict = fd)
-        return cost, predictions, trerr, tserr, summary
+        cost, trerr, tserr, summary = sess.run(
+            (self.cost, self.train_err, self.test_err, self.summary_op), feed_dict = fd)
+        return cost, trerr, tserr, summary
 
 
     def predictions(self, sess, input):
+        """Make predictions without back-propagation. """
+
         fd = {
-            self.input: input,
-            self.keep_prob: 1.0,
-            self.noise_prob: 0.0,
-            self.sap_prob: 0.0,
+            self.input:         input,
+            self.keep_prob:     1.0,
+            self.gaussian_prob: 0.0,
+            self.sap_prob:      0.0,
         }
         predictions = sess.run((self.reconstruction), feed_dict=fd)
         return predictions
 
 
     def hidden_activations(self, sess, input):
+        """ Obtain the values of the hidden layer, without back-propagation"""
         fd = {
-            self.input: input,
-            self.keep_prob: self.dropout_probability,
-            self.noise_prob: self.gaussian_prob,
-            self.sap_prob: self.salt_and_pepper_prob,
+            self.input:         input,
+            self.keep_prob:     self.keep_prob_val,
+            self.gaussian_prob: self.gaussian_prob_val,
+            self.sap_prob:      self.sap_prob_val,
         }
         hid = sess.run((self.hidden), feed_dict=fd)
         return hid
 
 
     def init_saver(self, more_vars, save_file):
+        """ Add support for saving model weights.
+        This should be called externally in order to specify additional variables
+        which are to be saved.
+        """
         var_list = list(self.weights.values()) + more_vars
         self.saver = tf.train.Saver(var_list=var_list)
         self.saver_filename = save_file
@@ -304,8 +332,14 @@ class DenoisingAutoencoder:
         return self.saver.restore(sess, self.saver_filename)
 
 
-    def rmse(self, preds, truth, mask, name):
-        """RMSE"""
+    def _rmse(self, preds, truth, mask, name):
+        """Calculate the RMSE of `preds` with respect to `truth`.
+        @param preds: predictions
+        @param truth: true values for which predictions were made
+        @param mask: boolean mask should be true when a value in the data is 
+                invalid (not to be considered)
+        @param name: how to name summaries related to the error
+        """
         notmask = tf.logical_not(mask)
         valid_preds = tf.boolean_mask(preds, notmask)
         valid_preds = valid_preds*2 + 3
@@ -316,3 +350,15 @@ class DenoisingAutoencoder:
         tf.summary.histogram(name + "_truth", valid_truth, collections=[self.name])
         err = tf.sqrt(tf.reduce_mean(tf.squared_difference(valid_truth, valid_preds)))
         return err
+
+
+    def get_rseed(self):
+        """Retrieve random seed.
+        To help obtain deterministic results we can use random seed. 
+        However, always using the same seed will cause problems with noise being
+        always applied to the same elements. Thus this function
+        """
+        if self.rseed is None:
+            return None
+        self.rseed += 1
+        return self.rseed
